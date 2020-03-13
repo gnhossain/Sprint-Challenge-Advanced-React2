407db48279242db67a152d2bb415165313890af5
gul hossain <spicymangojungle@gmail.com>
2020-03-13 09:29:29 -0700

    tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders "Players" text', () => {
+  const wrapper = render(<App />);
+  const hasTitleText = wrapper.queryByText(/women's world cup players/i);
 });
